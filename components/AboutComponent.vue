d8d43eec1c9a335654cfa7fd95e8f5b3e8c9882f
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2022-10-31 17:52:59 +0700

    push

---
+++ components/AboutComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <section id="about" class="about">
+    <div class="container-fluid">
+      <div class="row">
+        <div
+          class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch"
+          data-aos="fade-right"
+        >
+          <a
+            href="https://www.youtube.com/watch?v=StpBR2W8G5A"
+            class="glightbox play-btn mb-4"
+          ></a>
+        </div>
+
+        <div
+          class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
+          data-aos="fade-left"
+        >
+          <h3>Yuk kenalan dengan Xposi</h3>
+          <p>
+            Pendirian PT. Okta Sejahtera Insani, disingkat PT. OSI. pada waktu
+            akan menyelenggarakan kongres PERSI tahun 1993, Gedung balai sidang
+            baru saja digunakan setelah selesai direnovasi yang digunakan untuk
+            Penyenggaraan Pertemuan Non Blok Pada tahun 1992, sehingga pada
+            waktu itu sedang menjadi idaman bagi banyak organisasi untuk dapat
+            menyelenggarakan Kongres di gedung tersebut.
+          </p>
+          <p>
+            Dalam pertemuan informal pengurus PERSI dan IRSJAM yang dihadiri
+            oleh 8 orang anggota pengurus, disepakati gagasan bahwa
+            penyelenggaraan kongres PERSI tahun 1993 tidak diselenggarakan di
+            Gedung Manggala Wana Bhakti seperti sebelumnya, tetapi akan dicoba
+            diselenggarakan di Balai Sidang.
+          </p>
+          <a
+            href=""
+            class="btn btn-sm w-25 text-light"
+            style="background-color: rgb(23, 53, 199)"
+          >
+            Read More</a
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ partials/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<!-- <template>
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+        <h1><a href="index.html"><span>Xposi</span></a></h1>
+      </div>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#features">Features</a></li>
+          <li class="dropdown"><a href="#"><span>Event</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Healthy Event</a></li>              
+              </li>
+              <li><a href="#">IoT Event</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
+          <li><a class="nav-link scrollto" href="#recent-blog-posts">Newsblog</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+</template> -->
